Derivative calculation fix

pulser_diff/hamiltonian.py
@@ -21,7 +21,7 @@ from collections.abc import Mapping
 from typing import Union, cast
 
 import torch
-
+from torch import Tensor
 
 from pulser.backend.noise_model import NoiseModel
 from pulser.devices._device_datacls import BaseDevice
@@ -50,7 +50,7 @@ class Hamiltonian:
     def __init__(
         self,
         samples_obj: SequenceSamples,
-        qdict: dict[QubitId, torch.Tensor],
+        qdict: dict[QubitId, Tensor],
         device: BaseDevice,
         sampling_rate: float,
         config: NoiseModel,
@@ -60,13 +60,13 @@ class Hamiltonian:
         self._qdict = qdict
         self._device = device
         self._sampling_rate = sampling_rate
-        self._dist_list = []
+        self._dist_dict = {}
 
         # Type hints for attributes defined outside of __init__
         self.basis_name: str
         self._config: NoiseModel
-        self.op_matrix: dict[str, torch.Tensor]
-        self.basis: dict[str, torch.Tensor]
+        self.op_matrix: dict[str, Tensor]
+        self.basis: dict[str, Tensor]
         self.dim: int
         self._bad_atoms: dict[Union[str, int], bool] = {}
         self._doppler_detune: dict[Union[str, int], float] = {}
@@ -90,11 +90,11 @@ class Hamiltonian:
         self.operators: dict[str, defaultdict[str, dict]] = {
             addr: defaultdict(dict) for addr in ["Global", "Local"]
         }
-        self._collapse_ops: list[torch.Tensor] = []
+        self._collapse_ops: list[Tensor] = []
 
         self.set_config(config)
 
-    def _adapt_to_sampling_rate(self, full_array: torch.Tensor) -> torch.Tensor:
+    def _adapt_to_sampling_rate(self, full_array: Tensor) -> Tensor:
         """Adapt list to correspond to sampling rate."""
         indices = torch.linspace(
             0,
@@ -102,7 +102,7 @@ class Hamiltonian:
             int(self._sampling_rate * self._duration),
             dtype=int,
         )
-        return cast(torch.Tensor, full_array[indices])
+        return cast(Tensor, full_array[indices])
 
     @property
     def config(self) -> NoiseModel:
@@ -155,6 +155,8 @@ class Hamiltonian:
         """
         if not isinstance(cfg, NoiseModel):
             raise ValueError(f"Object {cfg} is not a valid `NoiseModel`.")
+        if len(cfg.noise_types) > 0:
+            raise NotImplementedError("Noisy simulations are not supported by pulser-diff")
         not_supported = (
             set(cfg.noise_types) - SUPPORTED_NOISES[self._interaction]
         )
@@ -191,7 +193,7 @@ class Hamiltonian:
 
         def add_noise(
             slot: _PulseTargetSlot,
-            samples_dict: Mapping[QubitId, dict[str, torch.Tensor]],
+            samples_dict: Mapping[QubitId, dict[str, Tensor]],
             is_global_pulse: bool,
         ) -> None:
             """Builds hamiltonian coefficients.
@@ -230,7 +232,7 @@ class Hamiltonian:
                             samples["Local"][basis][qid][qty] = 0.0
         self.samples = samples
 
-    def build_operator(self, operations: Union[list, tuple]) -> torch.Tensor:
+    def build_operator(self, operations: Union[list, tuple]) -> Tensor:
         """Creates an operator with non-trivial actions on some qubits.
 
         Takes as argument a list of tuples ``[(operator_1, qubits_1),
@@ -350,7 +352,7 @@ class Hamiltonian:
             self._update_noise()
         self._extract_samples()
 
-        def make_vdw_term(q1: QubitId, q2: QubitId) -> torch.Tensor:
+        def make_vdw_term(q1: QubitId, q2: QubitId) -> Tensor:
             """Construct the Van der Waals interaction Term.
 
             For each pair of qubits, calculate the distance between them,
@@ -358,13 +360,12 @@ class Hamiltonian:
             The units are given so that the coefficient includes a
             1/hbar factor.
             """
-            dist: torch.Tensor = torch.linalg.norm(self._qdict[q1] - self._qdict[q2])
-            dist.requires_grad_().retain_grad()
-            self._dist_list.append(dist)
-            U = 0.5 * self._device.interaction_coeff / dist**6
+            dist: Tensor = torch.linalg.norm(self._qdict[q1] - self._qdict[q2])
+            self._dist_dict[f"{q1}-{q2}"] = dist
+            U = (1.0+0.0j) * 0.5 * self._device.interaction_coeff / dist**6
             return self.build_operator([("sigma_rr", [q1, q2])]) * U
 
-        def make_xy_term(q1: QubitId, q2: QubitId) -> torch.Tensor:
+        def make_xy_term(q1: QubitId, q2: QubitId) -> Tensor:
             """Construct the XY interaction Term.
 
             For each pair of qubits, calculate the distance between them,
@@ -374,7 +375,7 @@ class Hamiltonian:
             """
             dist = torch.linalg.norm(self._qdict[q1] - self._qdict[q2])
             coords_dim = len(self._qdict[q1])
-            mag_field = cast(torch.Tensor, self.samples_obj._magnetic_field)[
+            mag_field = cast(Tensor, self.samples_obj._magnetic_field)[
                 :coords_dim
             ]
             mag_norm = torch.linalg.norm(mag_field)
@@ -395,7 +396,7 @@ class Hamiltonian:
                 [("sigma_ud", [q1]), ("sigma_du", [q2])]
             ) * U
 
-        def make_interaction_term(masked: bool = False) -> torch.Tensor:
+        def make_interaction_term(masked: bool = False) -> Tensor:
             if masked:
                 # Calculate the total number of good, unmasked qubits
                 effective_size = self._size - sum(self._bad_atoms.values())
@@ -555,7 +556,7 @@ class Hamiltonian:
 
         def H_t(t):
             # make sure that time is a tensor
-            if not isinstance(t, torch.Tensor):
+            if not isinstance(t, Tensor):
                 t = torch.tensor(t)
 
             # calculate time indices for interpolation
@@ -566,7 +567,7 @@ class Hamiltonian:
             ham = 2 * int_mat
             for det_mat, det_val in zip(det_matrices, det_values):
                 det = det_val[t_idx1] + (det_val[t_idx2] - det_val[t_idx1]) * (t - t_idx1 * dt) / dt
-                ham = (det_mat + det_mat.adjoint()) * det
+                ham += (det_mat + det_mat.adjoint()) * det
             for amp_mat, amp_val in zip(amp_matrices, amp_values):
                 amp = amp_val[t_idx1] + (amp_val[t_idx2] - amp_val[t_idx1]) * (t - t_idx1 * dt) / dt
                 ham += (amp_mat + amp_mat.adjoint()) * amp

pulser_diff/simresults.py
@@ -24,6 +24,7 @@ from typing import Mapping, Optional, Tuple, Union, cast
 
 import matplotlib.pyplot as plt
 import torch
+from torch import Tensor
 import numpy as np
 
 from pulser.result import Results, ResultType, SampledResult
@@ -43,7 +44,7 @@ class SimulationResults(ABC, Results[ResultType]):
     _use_pseudo_dens: bool = False
 
     def __init__(
-        self, size: int, basis_name: str, sim_times: torch.Tensor
+        self, size: int, basis_name: str, sim_times: Tensor
     ) -> None:
         """Initializes a new SimulationResults instance.
 
@@ -66,23 +67,23 @@ class SimulationResults(ABC, Results[ResultType]):
 
     @property
     @abstractmethod
-    def states(self) -> list[torch.Tensor]:
+    def states(self) -> list[Tensor]:
         """Lists states of the system at simulation times."""
         pass
 
     @abstractmethod
-    def get_state(self, t: float) -> torch.Tensor:
+    def get_state(self, t: float) -> Tensor:
         """Returns the state of the system at time t."""
         pass
 
     @abstractmethod
-    def get_final_state(self) -> torch.Tensor:
+    def get_final_state(self) -> Tensor:
         """Returns the final state of the system."""
         pass
 
     def expect(
-        self, obs_list: collections.abc.Sequence[torch.Tensor]
-    ) -> list[Union[float, complex, torch.Tensor]]:
+        self, obs_list: collections.abc.Sequence[Tensor]
+    ) -> list[Union[float, complex, Tensor]]:
         """Returns the expectation values of operators in obs_list.
 
         Args:
@@ -92,7 +93,7 @@ class SimulationResults(ABC, Results[ResultType]):
         Returns:
             Expectation values of obs_list.
         """
-        if not isinstance(obs_list, (list, torch.Tensor)):
+        if not isinstance(obs_list, (list, Tensor)):
             raise TypeError("`obs_list` must be a list of operators.")
 
         qobj_list = []
@@ -102,7 +103,7 @@ class SimulationResults(ABC, Results[ResultType]):
         legal_shape = (dim**self._size, dim**self._size)
         for obs in obs_list:
             if not (
-                isinstance(obs, torch.Tensor)
+                isinstance(obs, Tensor)
             ):
                 raise TypeError(
                     f"Incompatible type {type(obs)} of "
@@ -158,7 +159,7 @@ class SimulationResults(ABC, Results[ResultType]):
         """
         return self.sample_state(self._sim_times[-1], N_samples)
 
-    def plot(self, op: torch.Tensor, fmt: str = "", label: str = "") -> None:
+    def plot(self, op: Tensor, fmt: str = "", label: str = "") -> None:
         """Plots the expectation value of a given operator op.
 
         Args:
@@ -187,7 +188,7 @@ class SimulationResults(ABC, Results[ResultType]):
             )
 
     @lru_cache(maxsize=None)
-    def _calc_pseudo_density(self, t_index: int) -> torch.Tensor:
+    def _calc_pseudo_density(self, t_index: int) -> Tensor:
         """Calculates the pseudo-density matrix at a given time.
 
         The pseudo-density matrix is the diagonal matrix calculated from the
@@ -201,7 +202,7 @@ class SimulationResults(ABC, Results[ResultType]):
             The pseudo-density matrix as a Qobj.
         """
 
-        def _proj_from_bitstring(bitstring: str) -> torch.Tensor:
+        def _proj_from_bitstring(bitstring: str) -> Tensor:
             proj = dq.tensprod(
                 *[self._meas_projector(int(i)) for i in bitstring]
             )
@@ -213,7 +214,7 @@ class SimulationResults(ABC, Results[ResultType]):
             for i in torch.nonzero(w)[0]
         )
 
-    def _meas_projector(self, state_n: int) -> torch.Tensor:
+    def _meas_projector(self, state_n: int) -> Tensor:
         """Gets the post measurement projector.
 
         Args:
@@ -243,7 +244,7 @@ class NoisyResults(SimulationResults):
         run_output: typing.Sequence[SampledResult],
         size: int,
         basis_name: str,
-        sim_times: torch.Tensor,
+        sim_times: Tensor,
         n_measures: int,
     ) -> None:
         """Initializes a new NoisyResults instance.
@@ -276,7 +277,7 @@ class NoisyResults(SimulationResults):
         self._results = tuple(run_output)
 
     @property
-    def states(self) -> list[torch.Tensor]:
+    def states(self) -> list[Tensor]:
         """Measured states as a list of diagonal qutip.Qobj."""
         return [self.get_state(t) for t in self._sim_times]
 
@@ -285,7 +286,7 @@ class NoisyResults(SimulationResults):
         """Probability distribution of the bitstrings."""
         return [Counter(res.sampling_dist) for res in self]
 
-    def get_state(self, t: float, t_tol: float = 1.0e-3) -> torch.Tensor:
+    def get_state(self, t: float, t_tol: float = 1.0e-3) -> Tensor:
         """Gets the state at time t as a diagonal density matrix.
 
         Note:
@@ -303,7 +304,7 @@ class NoisyResults(SimulationResults):
         t_index = self._get_index_from_time(t, t_tol)
         return self._calc_pseudo_density(t_index)
 
-    def get_final_state(self) -> torch.Tensor:
+    def get_final_state(self) -> Tensor:
         """Get the final state of the simulation as a diagonal density matrix.
 
         Note:
@@ -317,7 +318,7 @@ class NoisyResults(SimulationResults):
 
     def plot(
         self,
-        op: torch.Tensor,
+        op: Tensor,
         fmt: str = ".",
         label: str = "",
         error_bars: bool = True,
@@ -338,7 +339,7 @@ class NoisyResults(SimulationResults):
         def get_error_bars() -> Tuple[ArrayLike, ArrayLike]:
             moy = self.expect([op])[0]
             standard_dev = cast(
-                torch.Tensor,
+                Tensor,
                 torch.sqrt(qutip.variance(op, self.states) / self.n_measures),
             )
             return moy, standard_dev
@@ -366,7 +367,7 @@ class CoherentResults(SimulationResults):
         run_output: typing.Sequence[DynamiqsResult],
         size: int,
         basis_name: str,
-        sim_times: torch.Tensor,
+        sim_times: Tensor,
         meas_basis: str,
         meas_errors: Optional[Mapping[str, float]] = None,
     ) -> None:
@@ -410,7 +411,7 @@ class CoherentResults(SimulationResults):
         self._meas_errors = meas_errors
 
     @property
-    def states(self) -> list[torch.Tensor]:
+    def states(self) -> list[Tensor]:
         """List of ``qutip.Qobj`` for each state in the simulation."""
         return torch.stack([res.state for res in self])
 
@@ -422,7 +423,7 @@ class CoherentResults(SimulationResults):
         tol: float = 1e-6,
         normalize: bool = True,
         t_tol: float = 1.0e-3,
-    ) -> torch.Tensor:
+    ) -> Tensor:
         """Get the state at time t of the simulation.
 
         Args:
@@ -459,7 +460,7 @@ class CoherentResults(SimulationResults):
         ignore_global_phase: bool = True,
         tol: float = 1e-6,
         normalize: bool = True,
-    ) -> torch.Tensor:
+    ) -> Tensor:
         """Returns the final state of the Simulation.
 
         Args:
@@ -490,7 +491,7 @@ class CoherentResults(SimulationResults):
             normalize,
         )
 
-    def _meas_projector(self, state_n: int) -> torch.Tensor:
+    def _meas_projector(self, state_n: int) -> Tensor:
         if self._meas_errors:
             err_param = (
                 self._meas_errors["epsilon"]