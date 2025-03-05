c22e1cd3572ec134a1e93ffe0b162aaa0b25b461
Vytautas Abramavicius <vytautas.abramavicius@pasqal.com>
2025-03-05 12:21:14 +0200

    fix latex rendering in docs

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,16 +1,8 @@
-window.MathJax = {
-    tex: {
-        inlineMath: [["\\(", "\\)"]],
-        displayMath: [["\\[", "\\]"]],
-        processEscapes: true,
-        processEnvironments: true
-    },
-    options: {
-        ignoreHtmlClass: ".*|",
-        processHtmlClass: "arithmatex"
-    }
-};
-
-document$.subscribe(() => {
-    MathJax.typesetPromise()
-})
+MathJax.Hub.Config({
+    "tex2jax": { inlineMath: [ [ '$', '$' ] ] }
+  });
+  MathJax.Hub.Config({
+    config: ["MMLorHTML.js"],
+    jax: ["input/TeX", "output/HTML-CSS", "output/NativeMML"],
+    extensions: ["MathMenu.js", "MathZoom.js"]
+  });
